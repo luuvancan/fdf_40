972b1655413e394fb254de68a1bab7309e67aeca
Van <luuvancan.it@gmail.com>
2018-05-04 11:48:31 +0700

    Init shopping cart

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
-Rails.application.config.assets.precompile += %w( responsiveslides.min.js owl.carousel.js SmoothScroll.min.js demo.js)
+Rails.application.config.assets.precompile += %w( responsiveslides.min.js owl.carousel.js SmoothScroll.min.js demo.js cart.js)
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'product/show'
 
   namespace :admin do
   resources :categories
   end
   root "static_pages#home"
   resources :product
+  resources :cart
 end
