ad727c36f68bfc36bbafefadfa419073e7e484e3
Van <luuvancan.it@gmail.com>
2018-05-04 10:33:20 +0700

    Init View Detail

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'product/show'
+
   namespace :admin do
   resources :categories
   end
   root "static_pages#home"
-  get "static_pages/home"
+  resources :product
 end
